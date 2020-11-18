18199785e07bb51041db58b688ccd51e888a983e
marceberhard <meberhard@me.com>
2020-11-18 09:34:10 +0100

    updatet execpolicy and readme

---
+++ both_general.ps1
@@ -0,0 +1,5 @@
+﻿### Show ExecutionPolicy for running PS Scripts/Commands
+Get-ExecutionPolicy
+
+### Show ExecutionPolicy for running PS Scripts/Commands on specific user
+Get-ExecutionPolicy -Scope CurrentUser
---
+++ clients_reports.ps1
@@ -0,0 +1,2 @@
+﻿#Battery Health Report on Client, Cycles, Battery Capacity, some History, designed Capacity
+powercfg /batteryreport /output "C:\Users\%username%\Desktop\report.html"
